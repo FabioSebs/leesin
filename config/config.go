68255dff64f7933b3d41aa8fcf3bb460525214b9
Fabio Espinoza <Fabrzy@AMTs-MacBook-Pro.local>
2023-05-22 13:48:12 +0700

    changes

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ type Config struct {
 func NewConfig() Config {
 	return Config{
 		FullDomain:     os.Getenv("FULLDOMAIN"),
-		AllowedDomains: []string{os.Getenv("ALLOWED1"), os.Getenv("ALLOWED2"), os.Getenv("ALLOWED3"), os.Getenv("ALLOWED4"), os.Getenv("ALLOWED5"), os.Getenv("ALLOWED6"), os.Getenv("FULLDOMAIN")},
+		AllowedDomains: []string{os.Getenv("ALLOWED1"), os.Getenv("ALLOWED2")},
 		MaxPage:        os.Getenv("MAXPAGE"),
 	}
 }
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,9 +1,11 @@
 package scraper
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"strconv"
-	"sync"
 
 	"github.com/FabioSebs/leesin/config"
 	"github.com/FabioSebs/leesin/logger"
@@ -25,50 +27,66 @@ func NewWebScraper() WebScraper {
 	env := config.NewConfig()
 
 	return &GoCollyProgram{
-		Collector: colly.NewCollector(),
-		Config:    env,
-		Logger:    logger.NewLogger(),
+		Collector: colly.NewCollector(colly.AllowedDomains(
+			env.AllowedDomains...,
+		)),
+		Config: env,
+		Logger: logger.NewLogger(),
 	}
 }
 
 func (g *GoCollyProgram) GetReviews() {
-
 	// Parsing Data to Model from URL
 	reviews := make([]Review, 0)
 
-	g.Collector.OnHTML(".data-business-unit-reviews-section", func(element *colly.HTMLElement) {
-		info := element.DOM
-
-		review := Review{
-			User:   info.Find(".data-consumer-name-typography").Text(),
-			Rating: "1",
-			Review: info.Find(".data-review-content").Text(),
-			Date:   info.Find(".data-service-review-date-of-experience-typography").Text(),
-		}
-		reviews = append(reviews, review)
+	g.Collector.OnHTML("div.styles_mainContent__nFxAv section.styles_reviewsContainer__3_GQw", func(element *colly.HTMLElement) {
+		element.ForEach("div.styles_cardWrapper__LcCPA", func(i int, h *colly.HTMLElement) {
+			review := Review{
+				User:   h.DOM.Find("div.styles_reviewCardInner__EwDq2 aside.styles_consumerInfoWrapper__KP3Ra div.styles_consumerDetailsWrapper__p2wdr a span").First().Text(),
+				Rating: "1",
+				Review: h.DOM.Find("div.styles_reviewCardInner__EwDq2 section.styles_reviewContentwrapper__zH_9M div.styles_reviewContent__0Q2Tg p.typography_body-l__KUYFJ").Text(),
+				Date:   h.DOM.Find("div.styles_reviewCardInner__EwDq2 section.styles_reviewContentwrapper__zH_9M div.styles_reviewContent__0Q2Tg p.typography_body-m__xgxZ_").Text(),
+			}
+			reviews = append(reviews, review)
+		})
 	})
 
 	// Request Feedback
 	g.Collector.OnRequest(func(r *colly.Request) {
-		g.Logger.WriteTrace(fmt.Sprintf("visiting url: %s", r.URL))
+		g.Logger.WriteTrace(fmt.Sprintf("visiting url: %s", r.URL.String()))
 	})
 
-	// Creating WaitGroups
-	var wg sync.WaitGroup
+	// Error Feedback
+	g.Collector.OnError(func(r *colly.Response, err error) {
+		g.Logger.WriteError(fmt.Sprintf("error: %s", err.Error()))
+	})
+
+	//Visiting URLS
+
 	jobNo, err := strconv.Atoi(g.Config.MaxPage)
 	if err != nil {
 		g.Logger.WriteError(err.Error())
 	}
-	wg.Add(jobNo)
 
-	//Visiting URLS
 	for i := 1; i <= jobNo; i++ {
-		go func(page string) {
-			defer wg.Done()
-			url := fmt.Sprintf(g.Config.FullDomain+"?page=%s&stars=1", page)
-			g.Collector.Visit(url)
-		}(strconv.Itoa(i))
+		page := strconv.Itoa(i)
+
+		url := fmt.Sprintf(g.Config.FullDomain+"?page=%s&stars=1", page)
+		if err := g.Collector.Visit(url); err != nil {
+			g.Logger.WriteError(err.Error())
+		}
 	}
-	wg.Wait()
+
 	utils.PrettyPrintStruct(reviews)
+	writeJSON(reviews)
+}
+
+func writeJSON(data []Review) {
+	file, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Println("Unable to create json file")
+		return
+	}
+
+	_ = ioutil.WriteFile("leaguereviews.json", file, 0644)
 }
